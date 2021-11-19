4b031b54d5263cd2c65f08734d0b8932b8fd95ab
duanhouhai <houhai.duan@mihoyo.com>
2021-11-19 20:19:20 +0800

    add ast

---
+++ proj/Ast/AstNode.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ast
+{
+	public class AstNode
+	{
+		public struct Position
+		{
+			public int line;
+			public int column;
+			public int offset;
+		}
+
+		public AstNode parent;
+		public List<AstNode> children;
+
+		public Position start;
+		public Position end;
+	}
+
+	public class DeclationNode:AstNode
+	{
+				
+	}
+
+	public class FunctionNode : AstNode
+	{
+	
+	}
+
+	public class FieldNode : AstNode
+	{
+		
+	}
+}
